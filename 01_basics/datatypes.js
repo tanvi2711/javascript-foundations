8fed59960f323f3a2da862667cb835e44c4eb880
tanvi2711 <tanvijivatode2711@gmail.com>
2026-02-13 21:21:37 +0530

    datatypes are learn in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")
+// alert(3+3)  We are using node js not browser 
+
+console.log(3+3)
+console.log("Tanvi")
+
+let name="tanvi"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number => 2 to power of 53
+// bigint => for big no
+// string  => ""
+// boolean => true/false
+// null => standalone value ex let state = null;
+// undefined => ex let state;
+// symbol => defining uniquness 
+
+// object 
+
+console.log(typeof 18);
+console.log(typeof state);
+console.log(typeof undefined);
+console.log(typeof null);//object
