ed562da492039785a6436a3b69e40bbf2e7a02b0
tanvi2711 <tanvijivatode2711@gmail.com>
2026-03-02 20:52:59 +0530

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,39 @@
+// let score=77
+// let score="77adc"
+// let score=null
+// let score=undefined
+let score=true
+
+console.log( typeof(score));
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+// NaN = Not a Number
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1 ; false=>0
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Tanvi"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=>true ; 0=>false
+// ""=>false
+// "tanvi"=>true
+
+
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
