09a8c7a4879eca9d86beef1c1b3ef28fdcf2ebf0
tanvi2711 <tanvijivatode2711@gmail.com>
2026-08-10 22:55:59 +0530

    overview reference_types in js

---
+++ refrence_types/function.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log("Hello world")
+}
+
+greet()
+
+
+function greet(name){
+    console.log("Hello world ",name)
+}
+
+greet("Tanvi")
+
+function greetPerson(name){
+    console.log("Hello world ",name)
+}
+
+greetPerson("Nishant")
+
+
+function square(num){
+    return num*num
+}
+
+let num=2
+console.log(square(num))
